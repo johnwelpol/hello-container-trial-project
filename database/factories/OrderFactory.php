cc632695e503b430cc0e807570229228971f1018
Johnwel <johnwellibariospol@gmail.com>
2022-10-28 14:41:02 +0800

    Allow to generate bl_number in factory.

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -15,9 +15,10 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
+            'bl_number'          => "BL-" . sprintf('%08d', rand(0, 100)),
             'bl_release_user_id' => User::all()->random()->id,
             'freight_payer_self' => false,
-            'contract_number' => '099999999',
+            'contract_number'    => '099999999',
         ];
     }
 }
