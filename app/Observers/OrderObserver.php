6e9e55c88582ab82da0ddd37aa8d5faf0f992dea
Johnwel <johnwellibariospol@gmail.com>
2022-10-28 14:42:29 +0800

    Add a updating listener when an order update and changes its bl_release_date from null to some value. Notify admin if this event happen.

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -3,6 +3,9 @@
 namespace App\Observers;
 
 use App\Models\order;
+use Illuminate\Support\Arr;
+use App\Services\AdminService;
+use App\Notifications\PaymentRequest;
 
 class OrderObserver
 {
@@ -14,19 +17,25 @@ class OrderObserver
      */
     public function created(order $order)
     {
-        $order->bl_number = "BL-" . sprintf('%08d', $order->id);
-        $order->save();
+
     }
 
     /**
-     * Handle the order "updated" event.
+     * Handle the order "updating" event.
      *
      * @param  \App\Models\order  $order
      * @return void
      */
-    public function updated(order $order)
+    public function updating(order $order)
     {
-        //
+        $original = $order->getOriginal();
+
+        if ($order->freight_payer_self == 0 &&
+            empty($original->bl_release_date) &&
+            Arr::has( $order->getDirty(), 'bl_release_date')
+        ) {
+            AdminService::notify(new PaymentRequest($order));
+        }
     }
 
     /**
